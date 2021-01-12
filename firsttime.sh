b15b6e6080902a121b8e0bd2a71a2da381d518ba
DryRun <david.renhwa.yu@gmail.com>
2021-01-12 08:47:05 -0600

    Add some scripts

--- firsttime.sh
+++ firsttime.sh
@@ -6,6 +6,7 @@ source ${LCG}/setup.sh
 # following https://aarongorka.com/blog/portable-virtualenv/, an alternative is https://github.com/pantsbuild/pex
 python -m venv --copies $NAME
 source $NAME/bin/activate
+export PATH=$PATH:$PWD/scripts
 python -m pip install setuptools pip --upgrade
 python -m pip install 'coffea==0.7.0rc1'
 python -m pip install xxhash
@@ -14,5 +15,7 @@ sed -i '40s/.*/VIRTUAL_ENV="$(cd "$(dirname "$(dirname "${BASH_SOURCE[0]}" )")" 
 sed -i '1s/#!.*python$/#!\/usr\/bin\/env python/' $NAME/bin/*
 sed -i "2a source ${LCG}/setup.sh" $NAME/bin/activate
 sed -i "4a source ${LCG}/setup.csh" $NAME/bin/activate.csh
+echo "export PATH=$PATH:$PWD/scripts" >> $NAME/bin/activate
+
 ipython kernel install --user --name=$NAME
 tar -zcf ${NAME}.tar.gz ${NAME}
