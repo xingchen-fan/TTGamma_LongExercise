93ca8a1d259369bd26dd02bc8a3dae67e155aac7
DryRun <david.renhwa.yu@gmail.com>
2021-01-12 13:00:23 -0600

    Remove PATH modifications

--- firsttime.sh
+++ firsttime.sh
@@ -6,7 +6,6 @@ source ${LCG}/setup.sh
 # following https://aarongorka.com/blog/portable-virtualenv/, an alternative is https://github.com/pantsbuild/pex
 python -m venv --copies $NAME
 source $NAME/bin/activate
-export PATH=$PATH:$PWD/scripts
 python -m pip install setuptools pip --upgrade
 #Use master branch of coffea to include jec patch
 python -m pip install git+https://github.com/CoffeaTeam/coffea.git@master
@@ -16,7 +15,6 @@ sed -i '40s/.*/VIRTUAL_ENV="$(cd "$(dirname "$(dirname "${BASH_SOURCE[0]}" )")" 
 sed -i '1s/#!.*python$/#!\/usr\/bin\/env python/' $NAME/bin/*
 sed -i "2a source ${LCG}/setup.sh" $NAME/bin/activate
 sed -i "4a source ${LCG}/setup.csh" $NAME/bin/activate.csh
-echo "export PATH=$PATH:$PWD/scripts" >> $NAME/bin/activate
 
 ipython kernel install --user --name=$NAME
 tar -zcf ${NAME}.tar.gz ${NAME}
