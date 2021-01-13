ddc4ca95c52465ad0be7c56269488d5581715fae
DryRun <david.renhwa.yu@gmail.com>
2021-01-13 08:26:49 -0600

    Condor settings for the group

--- runOnCondor.sh
+++ runOnCondor.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash
-
 tar -zxf ttgenv.tar.gz
 source ttgenv/bin/activate
 
 tar -zxf ttgamma.tar.gz
 
-python runFullDataset.py $1 --condor --chunksize 100000
+head runFullDataset.py
+python runFullDataset.py $1 --condor --chunksize 100000 --workers 2
 
+pwd
+ls -lrth
